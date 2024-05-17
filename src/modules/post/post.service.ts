5846f1993920c733fc291cc361ae0d074700bb6b
Matheus Henrique <mhps.matheus@gmail.com>
2024-05-16 22:25:53 -0300

    remove unnecessary import

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
-import { join } from 'path/posix'
 import { Post } from 'src/schemas/post.schema'
 
 type CreatePostInput = { id: string; text: string }
