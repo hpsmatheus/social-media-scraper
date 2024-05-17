60dc37a0d3ce9ae9072729e5651eb8b1210a8a8c
Matheus Henrique <mhps.matheus@gmail.com>
2024-05-16 22:27:25 -0300

    remove unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import ExampleModule from './modules/example/example.module'
 import TwitterMockModule from './modules/twitter-mock/twitter-mock.module'
 import ScraperModule from './modules/scraper/scraper.module'
 import { MongooseModule } from '@nestjs/mongoose'
@@ -9,7 +8,6 @@ import { MongooseModule } from '@nestjs/mongoose'
 	imports: [
 		ConfigModule.forRoot(),
 		MongooseModule.forRoot(process.env.MONGO_URL),
-		ExampleModule,
 		TwitterMockModule,
 		ScraperModule,
 	],
--- src/modules/example/example.service.ts
+++
@@ -1,7 +0,0 @@
-import ExampleResponse from 'src/typings/example/example.response.dto'
-
-export default class ExampleService {
-	public example(name: string): ExampleResponse {
-		return { message: `Hello, ${name} !` }
-	}
-}
